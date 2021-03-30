fded9681a4c2c0b21c87710d48ccaabd43f6b673
Maycon J Pereira <mayconjovan@gmail.com>
2021-03-29 21:05:15 -0300

    First commit

---
+++ src/test/java/com/maycon/TesteDevelcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maycon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDevelcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
